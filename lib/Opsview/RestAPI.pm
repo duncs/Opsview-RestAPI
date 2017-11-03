adf962a5571038940cd82ba700a958947ccaaaf7
Duncan Ferguson <duncan_j_ferguson@yahoo.co.uk>
2017-11-03 14:41:51 +0000

    Initial Commit

---
+++ lib/Opsview/RestAPI.pm
@@ -0,0 +1,18 @@
+use strict;
+use warnings;
+
+package Opsview::RestAPI;
+# ABSTRACT: this is the module abstract
+
+=head1 SYNOPSIS
+
+use Opsview::RestAPI;
+my @some_info='here;
+
+=head1 DESCRIPTION
+
+A description about the module
+
+=cut
+
+1;
